f60a77009a594c71ac9a1f223f2a17023847f441
Boaz Sender <boazeliezer@gmail.com>
2024-02-16 09:17:58 -0800

    update seed script to create a token for default user, and add migration

--- prisma/seed.ts
+++ prisma/seed.ts
@@ -1,13 +1,20 @@
+import { randomBytes } from "node:crypto";
+
 import { PrismaClient } from "@prisma/client";
 import bcrypt from "bcryptjs";
+import { sha256 } from "js-sha256";
 
 const prisma = new PrismaClient();
 
 async function seed() {
   const hashedPassword = await bcrypt.hash("letmeinplease", 10);
+  const newToken = randomBytes(16).toString("hex");
+  const hashedNewToken = sha256(newToken);
+
   const user = await prisma.user.create({
     data: {
       email: "boaz@bocoup.com",
+      token: hashedNewToken,
       password: {
         create: {
           hash: hashedPassword,
