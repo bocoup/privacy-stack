dcac540ed09d9b6ce5384228905365ef5710d1ac
Boaz Sender <boazeliezer@gmail.com>
2024-04-16 07:20:30 -0700

    switch indigo to slate

--- app/routes/_index.tsx
+++ app/routes/_index.tsx
@@ -135,7 +135,7 @@ export default function Index() {
             <div className="mt-10 flex items-center justify-center gap-x-6">
               <Link
                 to="/auth/join"
-                className="rounded-md bg-slate-600 px-3.5 py-2.5 text-sm font-semibold text-white shadow-sm hover:bg-indigo-500 focus-visible:outline focus-visible:outline-2 focus-visible:outline-offset-2 focus-visible:outline-indigo-600"
+                className="rounded-md bg-slate-600 px-3.5 py-2.5 text-sm font-semibold text-white shadow-sm hover:bg-slate-500 focus-visible:outline focus-visible:outline-2 focus-visible:outline-offset-2 focus-visible:outline-indigo-600"
               >
                 Sign up
               </Link>
