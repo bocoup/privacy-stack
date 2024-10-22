826d146245682e79c472187eee6e2caf0651df15
Boaz Sender <boazeliezer@gmail.com>
2024-10-22 12:33:41 -0700

    fix smoke test config

--- cypress.config.ts
+++ cypress.config.ts
@@ -5,22 +5,12 @@ export default defineConfig({
     setupNodeEvents: (on, config) => {
       const isDev = config.watchForFileChanges;
       const port = process.env.PORT ?? (isDev ? "3000" : "8811");
-      const configOverrides: Partial<Cypress.PluginConfigOptions> = {
+
+      return {
+        ...config,
         baseUrl: `http://localhost:${port}`,
         screenshotOnRunFailure: !process.env.CI,
       };
-
-      // To use this:
-      // cy.task('log', whateverYouWantInTheTerminal)
-      on("task", {
-        log: (message) => {
-          console.log(message);
-
-          return null;
-        },
-      });
-
-      return { ...config, ...configOverrides };
     },
   },
 });
